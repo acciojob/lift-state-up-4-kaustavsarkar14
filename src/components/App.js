0bb965f24a98154b395191257e4a51dfe586d7ce
kaustavsarkar14 <sarkar.kaustav14@gmail.com>
2023-11-17 04:29:31 -0800

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,45 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
+const Child = ({items, setItems})=>{
+  function deleteItem(i){
+    const newItems = items.filter((el,ind)=>ind!=i)
+    setItems(newItems)
+  }
+  return (
+    <div className="child">
+      <ul>
+        {
+          items.map((el,i)=>{
+            return (
+              <li>{el.name} {el.price} <button onClick={()=>deleteItem(i)}  >Delete</button></li>
+            )
+          })
+        }
+      </ul>
+    </div>
+  )
+}
+
+
 const App = () => {
+  const [items ,setItems] = useState([])
+  const [newItemName ,setNewItemName] = useState('')
+  const [newItemPrice ,setNewItemPrice] = useState(0)
+  console.log(items)
   return (
     <div>
         {/* Do not remove the main div */}
+        <div className="parent">
+          <label htmlFor="itemName">Item Name</label>
+          <input id="itemName" type="text" onChange={e=>setNewItemName(e.target.value)} />
+          <label htmlFor="itemPrice">Item Price</label>
+          <input id="itemPrice" type="number" onChange={e=>setNewItemPrice(e.target.value)} />
+          <button onClick={()=>setItems([...items,{name:newItemName, price:newItemPrice}])} >Add Items</button>
+
+            <Child items={items} setItems={setItems} />
+        </div>
     </div>
   )
 }
